87264bfe0950b2184a4ed1385a140dd2f53e8bda
Brandon Semilla <semibran@gmail.com>
2022-01-24 11:13:42 -0800

    generate patterns with no double digits

--- Lab3.java
+++ Lab3.java
@@ -11,20 +11,40 @@ public class Lab3 {
         patterns.add(Integer.toString(i));
       }
       return patterns;
-    } else {
-      ArrayList<String> patterns = new ArrayList<>();
-      ArrayList<String> patternsPrev = generateAllPatterns(n - 1);
-      for (int i = 0; i < patternsPrev.size(); i++) {
-        for (int j = 0; j < 3; j++) {
-          patterns.add(patternsPrev.get(i) + Integer.toString(j));
+    }
+
+    ArrayList<String> patterns = new ArrayList<>();
+    ArrayList<String> patternsPrev = generateAllPatterns(n - 1);
+    for (int i = 0; i < patternsPrev.size(); i++) {
+      for (int j = 0; j < 3; j++) {
+        patterns.add(patternsPrev.get(i) + Integer.toString(j));
+      }
+    }
+    return patterns;
+  }
+
+  public static ArrayList<String> generatePatternsWithNoDoubleDigits(int n) {
+    if (n == 1) {
+      return generateAllPatterns(n);
+    }
+
+    ArrayList<String> patterns = new ArrayList<>();
+    ArrayList<String> patternsPrev = generatePatternsWithNoDoubleDigits(n - 1);
+    for (int i = 0; i < patternsPrev.size(); i++) {
+      String prevPattern = patternsPrev.get(i);
+      for (int j = 0; j < 3; j++) {
+        char lastCharOfPrevPattern = prevPattern.charAt(prevPattern.length() - 1);
+        char intAsChar = Integer.toString(j).charAt(0);
+        if (lastCharOfPrevPattern != intAsChar) {
+          patterns.add(prevPattern + intAsChar);
         }
       }
-      return patterns;
     }
+    return patterns;
   }
 
   public static void main(String[] args) {
-    ArrayList<String> patterns = generateAllPatterns(3);
+    ArrayList<String> patterns = generatePatternsWithNoDoubleDigits(3);
     System.out.println(patterns);
   }
 }
