5e5afc4329532d77aecd1376435ee0b5ce9fa13a
Brandon Semilla <semibran@gmail.com>
2022-01-24 11:03:34 -0800

    switch to array list

--- Lab3.java
+++ Lab3.java
@@ -1,20 +1,22 @@
 package lab3;
 
+import java.util.ArrayList;
+
 public class Lab3 {
 
-  public static String[] generateAllPatterns(int n) {
+  public static ArrayList<String> generateAllPatterns(int n) {
     if (n == 1) {
-      String[] patterns = new String[3];
+      ArrayList<String> patterns = new ArrayList<>();
       for (int i = 0; i < 3; i++) {
-        patterns[i] = Integer.toString(i);
+        patterns.add(Integer.toString(i));
       }
       return patterns;
     } else {
-      String[] patterns = new String[(int) Math.pow(3, n)];
-      String[] patternsPrev = generateAllPatterns(n - 1);
-      for (int i = 0; i < patternsPrev.length; i++) {
+      ArrayList<String> patterns = new ArrayList<>();
+      ArrayList<String> patternsPrev = generateAllPatterns(n - 1);
+      for (int i = 0; i < patternsPrev.size(); i++) {
         for (int j = 0; j < 3; j++) {
-          patterns[i * 3 + j] = patternsPrev[i] + Integer.toString(j);
+          patterns.add(patternsPrev.get(i) + Integer.toString(j));
         }
       }
       return patterns;
@@ -22,9 +24,7 @@ public class Lab3 {
   }
 
   public static void main(String[] args) {
-    String[] patterns = generateAllPatterns(3);
-    for (int i = 0; i < patterns.length; i++) {
-      System.out.println(patterns[i]);
-    }
+    ArrayList<String> patterns = generateAllPatterns(3);
+    System.out.println(patterns);
   }
 }
